f02cc4185a071a7993b9dcf0322434ead6ece9cf
Wolfgang Lumetsberger <Wolfgang.Lumetsberger@smartpoint.at>
2023-01-24 22:12:19 +0100

    - add basic blazor serverside app
    - add authentication with auth0

---
+++ oervmariatrost-kursbuch/Program.cs
@@ -0,0 +1,87 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using oervmariatrost_kursbuch.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+    .AddCookie()
+    .AddOpenIdConnect("Auth0", options => {
+        options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}";
+
+        options.ClientId = builder.Configuration["Auth0:ClientId"];
+        options.ClientSecret = builder.Configuration["Auth0:ClientSecret"];
+
+        options.ResponseType = OpenIdConnectResponseType.Code;
+
+        options.Scope.Clear();
+        options.Scope.Add("openid");
+        options.Scope.Add("profile"); // <- Optional extra
+        options.Scope.Add("email");   // <- Optional extra
+
+        options.CallbackPath = new PathString("/callback");
+        options.ClaimsIssuer = "Auth0";
+        options.SaveTokens = true;
+
+        options.TokenValidationParameters = new()
+        {
+            NameClaimType = "name",
+        };
+        // Add handling of lo
+        options.Events = new OpenIdConnectEvents
+        {
+            OnRedirectToIdentityProviderForSignOut = (context) =>
+            {
+                var logoutUri = $"https://{builder.Configuration["Auth0:Domain"]}/v2/logout?client_id={builder.Configuration["Auth0:ClientId"]}";
+
+                var postLogoutUri = context.Properties.RedirectUri;
+                if (!string.IsNullOrEmpty(postLogoutUri))
+                {
+                    if (postLogoutUri.StartsWith("/"))
+                    {
+                        var request = context.Request;
+                        postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                    }
+                    logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
+                }
+
+                context.Response.Redirect(logoutUri);
+                context.HandleResponse();
+
+                return Task.CompletedTask;
+            }
+        };
+    });
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+app.MapRazorPages();
+app.Run();
